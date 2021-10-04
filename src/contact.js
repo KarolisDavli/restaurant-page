53e992d175ac89c9d1c9cdfe85bd80143b9dfd1b
Karolis Davli <kdavlijevas@gmail.com>
2021-10-04 22:05:17 +0200

    generate menu

--- src/contact.js
+++ src/contact.js
@@ -8,14 +8,30 @@ const contact = () => {
 
   const menuContent = document.createElement('div');
 
-  heading2.innerText = 'Contact';
+  heading2.innerText = 'Get in touch';
 
-
-  para.innerText = 'Get in touch';
 
   menuContent.append(heading2, para);
   content.append(menuContent);
-  console.log('this is menu');
+
+
+  const info = [
+    ['Address', 'Grilled Halloumi with a cherry tomato \u0026 chutney & croûtes.'],
+    ['Phone', '+370 69224808'],
+    ['Email', 'hello@foodlab.com'],
+    ['We open', 'Monday - Thursday     09:00 - 23:00 Friday - Saturday     09:00 - 01:00 Sunday     09:00 - 13:00']
+  ];
+
+  const infoRender = document.createElement('div');
+  const bigInfo = document.createElement('h4');
+  const smallIfo = document.createElement('p');
+
+  info.forEach(function reder(i) {
+
+    console.log(i);
+  }) 
+
+  
 }
 
 export default contact;
--- src/menu.js
+++ src/menu.js
@@ -4,42 +4,8 @@ import {
   para,
 } from "./index.js";
 
+
 const menuContent = document.createElement('div');
-
-const meals = {
-  name: ['Grilled Halloumi', 'POT OF MUSSELS', 'FALAFEL PATTIES', 'JAMAICAN MEAT BALLS'],
-  desc: ['Grilled Halloumi with a cherry tomato chutney \u0026 croûtes.',
-        'Small portion of fresh Sicilian mussels,cooked in white wine, anchovy butter, lemon and black pepper.',
-        'Homemade chickpea patties served with raita dressing.',
-        'Spiced Jamaican beef patties served with tomato chutney.']
-};
-
-// Vietoj object sukurt array ir pervaryt per ji su forEach ir kas kast appending contenta
-
-
-function go() {
-  const starters = document.createElement('div');
-  starters.classList.add('mealItems');
-
-  for (let i = 0; i < meals.name.length; i++) {
-    let mealName = document.createElement('h4');
-    mealName.classList.add('meal-name');
-    mealName = meals.name[i];
-    starters.append(mealName);
-    
-    let mealDesc = document.createElement('p');
-    mealDesc.classList.add('meal-desc');
-    mealDesc = meals.desc[i];
-    starters.append(mealDesc);
-    menuContent.append(starters);
-    
-    console.log(starters);
-    // starters.append(mealName, mealDesc); 
-    // menuContent.append(starters);
-  }
-}
-
-
 
 const menu = () => {
 
@@ -47,17 +13,36 @@ const menu = () => {
 
   para.innerText = 'Our food is cooked fresh and based on fresh ingredients and produce, during busy hours waiting times may be longer than we’d like them to be. After all we all know, good food takes time ';
 
-  const starters = go();
+
   menuContent.append(heading2, para);
   content.append(menuContent);
 
+
+
+
+  createMenuItem('Grilled Halloumi', 'Grilled Halloumi with a cherry tomato chutney & croûtes.')
+  createMenuItem('POT OF MUSSELS', 'Small portion of fresh Sicilian mussels, cooked in white wine, anchovy butter, lemon and black pepper.');
+  createMenuItem('FALAFEL PATTIES ', 'Homemade chickpea patties served with raita dressing.')
+  createMenuItem('JAMAICAN MEAT BALLS', 'Spiced Jamaican beef patties served with tomato chutney.')
 }
 
-// const createStartersMenu = () => {
-//   const h3 = document.createElement('h3');
-//   h3.innerText('Starters');
-// }
 
+function createMenuItem(name, desc) {
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('menu-item');
+
+    const itemName = document.createElement('h4');
+    itemName.classList.add('item-name');
+    itemName.innerHTML = name;
+
+    const itemDesc = document.createElement('p');
+    itemDesc.classList.add('item-desc');
+    itemDesc.innerHTML = desc;
+
+    menuItem.append(itemName, itemDesc);
+    menuContent.append(menuItem);
+    console.log(menuContent);
+};
 
 
 
